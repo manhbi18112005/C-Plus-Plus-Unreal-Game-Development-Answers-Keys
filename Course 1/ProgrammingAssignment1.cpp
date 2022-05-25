ca4f48cf416054ac5ef49d6b6b2dd92f41522b85
๖ۣۜŇŇ_MyTツ <myt@nonamestudio.xyz>
2022-05-25 14:19:22 +0700

    updated

---
+++ Course 1/ProgrammingAssignment1.cpp
@@ -0,0 +1,33 @@
+// Copyright A.T. Chamillard. All Rights Reserved.
+
+#include <iostream>
+#include <string>
+
+// IMPORTANT: Only add code in the section
+// indicated below. The code I've provided
+// makes your solution work with the 
+// automated grader on Coursera
+
+/**
+ * Programming Assignment 1
+ * @return exit status
+*/
+int main()
+{
+	// loop while there's more input
+	std::string Input;
+	std::getline(std::cin, Input);
+	while (Input[0] != 'q')
+	{
+		// Add your code between this comment
+		// and the comment below. You can of
+		// course add more space between the
+		// comments as needed
+
+
+
+		// Don't add or modify any code below
+		// this comment
+		std::getline(std::cin, Input);
+	}
+}
