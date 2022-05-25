7ee5c3cfb0cec57dd2ad258f654612a430cb203a
๖ۣۜŇŇ_MyTツ <myt@nonamestudio.xyz>
2022-05-25 14:22:23 +0700

    Update ProgrammingAssignment1.cpp

--- Course 1/ProgrammingAssignment1.cpp
+++ Course 1/ProgrammingAssignment1.cpp
@@ -5,7 +5,7 @@
 
 // IMPORTANT: Only add code in the section
 // indicated below. The code I've provided
-// makes your solution work with the 
+// makes your solution work with the
 // automated grader on Coursera
 
 /**
@@ -23,7 +23,7 @@ int main()
 		// and the comment below. You can of
 		// course add more space between the
 		// comments as needed
-
+        std::cout << "H" << std::endl << "He" << std::endl << "Li" << std::endl << "Be" << std::endl << "B" << std::endl << "C" << std::endl << "N" << std::endl << "O" << std::endl << "F" << std::endl << "Ne" << std::endl;
 
 
 		// Don't add or modify any code below
